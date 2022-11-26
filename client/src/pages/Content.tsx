74dfdb32ca3772f8c2b0d0576e3e14846fd8ed83
Matt5246 <116891485+Matt5246@users.noreply.github.com>
2022-11-26 17:06:49 +0100

    modified:   client/src/App.tsx 	new file:   client/src/components/Header.tsx 	new file:   client/src/components/Navbar.tsx 	new file:   client/src/pages/Content.tsx

---
+++ client/src/pages/Content.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Content(props:any) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+Content.propTypes = {
+
+}
+
+export default Content
+
